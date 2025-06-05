86ee4b32727aa946a11256a6e2a11cd925a061f9
Hardik Modha <modha.hardik27@gmail.com>
2025-06-05 17:55:49 +0530

    Initial Commit

---
+++ GroceryApp/GroceryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryAppApp.swift
+//  GroceryApp
+//
+//  Created by Hardik Modha on 05/06/25.
+//
+
+import SwiftUI
+
+@main
+struct GroceryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
